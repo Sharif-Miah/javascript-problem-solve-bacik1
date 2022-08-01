7af8cd6ccd1a6eab851b4b17d904c84139f463cd
farhan sharif <farhansharif1410@gmail.com>
2022-08-01 19:24:36 -0230

    first plm solve

---
+++ index.js
@@ -0,0 +1,14 @@
+// Practice Problem 1 
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+
+// a) Find the index of "Banana" and replace 'Banana' with 'Mango'.
+// b) Remove 'Orange' and add 'Watermwlon'.
+
+// solving bleow :
+
+// a) 
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+var findIndex = fruits.indexOf('Banana')
+console.log(findIndex);
